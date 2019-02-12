039ccf138b60a98673c1927542f282cff26b946c
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-12 18:38:33 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,38 @@
+require 'pry'
 def get_first_name_of_season_winner(data, season)
   # code here
+  i = 0
+  str = ""
+  data.each do |key, val|
+    #binding.pry
+    if key == season
+      while i < val.length
+        if val[i]["status"] == "Winner"
+          str = val[i]["name"].split(' ')
+          return str[0]
+        end
+        i += 1
+      end
+    end
+  end
+
 end
 
-def get_contestant_name(data, occupation)
+def get_contestant_name(data, occupation) #data = { season: [array of hashes]}
   # code here
+  i = 0
+  str = ""
+  data.each do |key, val| #key = season, val = [{}, {}, {}]
+    #binding.pry
+    while i < val.length # i < 30
+      if val[i]["occupation"] == occupation
+        str = val[i]["name"]
+        return str
+      end
+      i += 1
+    end
+  end
+  
 end
 
 def count_contestants_by_hometown(data, hometown)
